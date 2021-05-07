1f72ef6f54465745035876af5a1288de83d87494
GUTT-JuanVazquez <71057053+GUTT-JuanVazquez@users.noreply.github.com>
2021-05-07 04:06:12 -0500

    Añadiendo socket daltantes

---
+++ app/Controllers/Ws/CocheraController.js
@@ -0,0 +1,16 @@
+'use strict'
+
+class CocheraController {
+  constructor ({ socket, request }) {
+    this.socket = socket
+    this.request = request
+  }
+
+  onMessage(data){
+    console.log("Chat de cochera")
+    this.socket.broadcastToAll("message",data)
+    console.log(data)
+  }
+}
+
+module.exports = CocheraController
--- start/socket.js
+++ start/socket.js
@@ -17,3 +17,14 @@ const Ws = use('Ws')
 
 //Channel Focos
 Ws.channel('wsfoco','FocoController')
+
+//Channel Humedad
+Ws.channel('wshumedad','FocoController')
+
+//Channel Temperatura
+Ws.channel('wstemperatura','FocoController')
+
+//Channel Cochera
+Ws.channel('wscochera','FocoController')
+
+
